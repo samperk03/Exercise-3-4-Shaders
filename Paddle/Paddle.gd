5654cb39ba683f909b8eff9c246f79128916642c
samperk03 <142915025+samperk03@users.noreply.github.com>
2023-10-11 18:22:31 -0400

    completes requirements for ex. 3-4

--- Paddle/Paddle.gd
+++ Paddle/Paddle.gd
@@ -13,9 +13,24 @@ func _ready():
 	width = $CollisionShape2D.get_shape().get_size().x
 	target = Vector2(Global.VP.x / 2, Global.VP.y - 80)
 
+
 func _physics_process(_delta):
 	target.x = clamp(target.x, 0, Global.VP.x - 2*width)
 	position = target
+	var ball_container = get_node_or_null("/root/Game/Ball_Container")
+	if ball_container != null and ball_container.get_child_count() > 0:
+		var ball = ball_container.get_child(0)
+		$Eye1/Pupil/Sprite.position.x = 7
+		$Eye2/Pupil/Sprite.position.x = 7
+		$Eye1/Pupil.look_at(ball.position)
+		$Eye2/Pupil.look_at(ball.position)
+		var d = ((($Mouth.global_position.y - ball.global_position.y)/Global.VP.y)-0.2)*2
+		d = clamp(d, -1, 1)
+		$Mouth.scale.y = d
+	else:
+		$Eye1/Pupil/Sprite.position.x = 0
+		$Eye2/Pupil/Sprite.position.x = 0
+		$Mouth.scale.y = 1
 
 func _input(event):
 	if event is InputEventMouseMotion:
